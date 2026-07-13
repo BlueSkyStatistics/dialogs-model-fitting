1add5a1bfd7908a4c432ab6602abd1de4c7bfb25
AnilD <anildabralpro@gmail.com>
2026-07-13 13:35:00 +0530

    empty entries added to get menu-dropdowns in alphabetical order, for new dialogs from Ross added to professional.

--- nav.js
+++ nav.js
@@ -18,6 +18,20 @@ const nav = [
                     "./contrastsSet"
                 ]
             },
+			{
+                "name": "Elastic Net",
+                "icon": "icon-variance",
+                "children": [
+    
+                ]				
+			},		
+			{
+                "name": "GEE",
+                "icon": "icon-link",
+                "children": [
+                 
+                ]				
+			},			
             "./glzm",
             {
                 "name": "IRT",
